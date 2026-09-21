6fa96486cda709f307417f582bce5af21c214de2
rumedia-regions <vatline.90@gmail.com>
2026-09-21 11:21:53 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.CCR_CONFIG = Object.freeze({
-  // После deployment Apps Script вставьте сюда URL, который заканчивается на /exec.
-  // Пример: https://script.google.com/macros/s/XXXXXXXX/exec
-  appsScriptExecUrl: "PASTE_APPS_SCRIPT_EXEC_URL_HERE"
+  appsScriptExecUrl: "https://script.google.com/macros/s/AKfycbyKBAJFHt8aWHreAPUjMBJ560BMXKcPnXlpoqD9pjLG4UesAnC4yK8w93r82xXby-xOlQ/exec"
 });
